a084ce77653edc7bf8aa87be8a4a959ae78751b2
Bevan Weir <WeirB@landcare.ad.landcareresearch.co.nz>
2016-03-27 14:20:59 +1300

    minor updates

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ R code to produce stats of specimen data from https://scd.landcareresearch.co.nz
 
 To download data you must have an account, you can signup here: https://scd.landcareresearch.co.nz/Account/Register
 
+The exampe data is only from the ICMP collection only searching for "root*"
+
 Current bugs:
-The first column of SCDSpecimens.csv needs to be removed. Primary key is the next collumn.
+The first column of SCDSpecimens.csv needs to be removed (from downloaded file, it is fixed in this example). Primary key is the next collumn.
--- Rscript.R
+++ Rscript.R
@@ -21,6 +21,27 @@ p <- ggplot(SCD.full, aes(Country_CE1)) + labs(title = "isolates by country") + 
 p <- p + theme(axis.text.x=element_text(angle=-90, hjust=0))
 p + geom_bar()+ coord_flip()
 print_bars <- p + geom_bar()+ coord_flip()
+ggsave(print_bars, file='ICMP_country.pdf', width=10, height=10)
+
+#ggplot code for price category
+attach(SCD.full) #this means we don't need the $ sign
+require(ggplot2)
+p <- ggplot(SCD.full, aes(Flags)) + labs(title = "isolates by country") + labs(x = "Taxon", y = "number of isolates")
+p <- p + theme(axis.text.x=element_text(angle=-90, hjust=0))
+p + geom_bar()+ coord_flip()
+print_bars <- p + geom_bar()+ coord_flip()
 ggsave(print_bars, file='ICMP_kingdoms.pdf', width=10, height=10)
 
-#git chnge test
+
+#summarise the hosts
+summary(PreferredName_C2)
+
+#ggplot code for host (too many)
+attach(SCD.full) #this means we don't need the $ sign
+require(ggplot2)
+p <- ggplot(SCD.full, aes(TaxonName_C2)) + labs(title = "isolates by country") + labs(x = "Taxon", y = "number of isolates")
+p <- p + theme(axis.text.x=element_text(angle=-90, hjust=0))
+p + geom_bar()+ coord_flip()
+print_bars <- p + geom_bar()+ coord_flip()
+ggsave(print_bars, file='ICMP_kingdoms.pdf', width=10, height=10)
+
