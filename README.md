c9a16d4bf7211543311ceffb3a4e648bf33ce856
Bevan Weir <WeirB@landcareresearch.co.nz>
2022-09-18 22:48:15 +1200

    updated with september 2022 export format changes

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ R code to produce stats of specimen data from https://scd.landcareresearch.co.nz
 To download data you must have an account, you can signup here: https://scd.landcareresearch.co.nz/Account/Register
 
 The example data is from the ICMP collection only searching for "root*" (i.e. mostly organisms isolates from plant roots).
+
+Updated September 2022 with a new export format
--- Rscript.R
+++ Rscript.R
@@ -14,7 +14,7 @@ library(readr)
 
 #============ Load data from csv files ================
 
-SCD.df <- read_csv("SCDSpecimens2.csv")
+SCD.df <- read_csv("SCDSpecimens.csv")
 head(SCD.df)
 tail(SCD.df)
 summary(SCD.df, maxsum=10)
@@ -22,67 +22,25 @@ summary(SCD.df, maxsum=10)
 SCD.df %>% 
   glimpse
 
-#the export file is borked. non standard csv file
+#============ Simple graphs ================
+
+#Simple sample type barchart
+ggplot(SCD.df, aes(SpecimenType)) +
+  labs(title = "ICMP cultures from roots by SpecimenType") +
+  labs(x = "Taxon", y = "number of isolates") + 
+  theme(axis.text.x=element_text(angle=-90, hjust=0)) +
+  geom_bar() + 
+  coord_flip()
+ggsave(file='./ICMP_specimen_types.png', width=7, height=7)
 
 
-# OK the actual problem is the export file. Try "leaf instead???
+#country
+ggplot(SCD.df, aes(VerbatimCountry_CE1)) +  #note Country_CE1 is blank
+  labs(title = "isolates by country") +
+  labs(x = "Taxon", y = "number of isolates") + 
+  theme(axis.text.x=element_text(angle=-90, hjust=0)) +
+  geom_bar() + 
+  coord_flip()
+ggsave(file='./ICMP_countries.png', width=7, height=7)
 
-## --------------------- Load data -----------------------------------
-SCD.full <- read.csv("SCDSpecimens.csv", row.names=NULL) #row.names=NULL ignores the first column
-head(SCD.full)
-summary(SCD.full, maxsum=40)
-
-#summarise the bacteria or fungi
-summary(PreferredName_C1)
-
-#summarise the hosts
-summary(PreferredName_C2)
-
-#ggplot code for kingdom
-attach(SCD.full) #this means we don't need the $ sign
-require(ggplot2)
-p <- ggplot(SCD.full, aes(SpecimenType)) + labs(title = "ICMP by Kingdom") + labs(x = "Taxon", y = "number of isolates")
-p <- p + theme(axis.text.x=element_text(angle=-90, hjust=0))
-p + geom_histogram()+ coord_flip()
-print_bars <- p + geom_histogram()+ coord_flip()
-ggsave(print_bars, file='ICMP_kingdoms.pdf', width=10, height=10)
-
-#plain code for a kingdom barchart
-attach(ICMP.dump) 
-require(ggplot2)
-p <- ggplot(ICMP.dump, aes(SpecimenType)) + labs(title = "Cultures in the ICMP by Kingdom") + labs(x = "Taxon", y = "number of isolates")
-p <- p + theme(axis.text.x=element_text(angle=-90, hjust=0))
-p + geom_bar()+ coord_flip()
-print_bars <- p + geom_bar()+ coord_flip()
-ggsave(print_bars, file='ICMP_kingdoms.png', width=7, height=7)
-
-#ggplot code for country
-attach(SCD.full) #this means we don't need the $ sign
-require(ggplot2)
-p <- ggplot(SCD.full, aes(Country_CE1)) + labs(title = "isolates by country") + labs(x = "Taxon", y = "number of isolates")
-p <- p + theme(axis.text.x=element_text(angle=-90, hjust=0))
-p + geom_histogram()+ coord_flip()
-print_bars <- p + geom_histogram()+ coord_flip()
-ggsave(print_bars, file='ICMP_country.pdf', width=10, height=10)
-
-#ggplot code for price category
-attach(SCD.full) #this means we don't need the $ sign
-require(ggplot2)
-p <- ggplot(SCD.full, aes(Flags)) + labs(title = "isolates by country") + labs(x = "Taxon", y = "number of isolates")
-p <- p + theme(axis.text.x=element_text(angle=-90, hjust=0))
-p + geom_histogram()+ coord_flip()
-print_bars <- p + geom_histogram()+ coord_flip()
-ggsave(print_bars, file='ICMP_kingdoms.pdf', width=10, height=10)
-
-
-#ggplot code for host (too many)
-attach(SCD.full) #this means we don't need the $ sign
-require(ggplot2)
-p <- ggplot(SCD.full, aes(TaxonName_C2)) + labs(title = "isolates by country") + labs(x = "Taxon", y = "number of isolates")
-p <- p + theme(axis.text.x=element_text(angle=-90, hjust=0))
-p + geom_histogram()+ coord_flip()
-print_bars <- p + geom_histogram()+ coord_flip()
-ggsave(print_bars, file='ICMP_kingdoms.pdf', width=10, height=10)
-
-#minor test github update
 
